0acc9ef356dd7be63ec6ea830aba6eb701d191d3
Egor Smirnov <egor@avivalinks.com>
2023-10-19 08:33:02 +0400

    [CRC] Update .f file.

--- crc/rtl/sim/crc_tb.f
+++ crc/rtl/sim/crc_tb.f
@@ -1,6 +1,7 @@
 ./crc_byte_enable_tb.sv
-./crc_byte_enable.sv
-./crc_parallel.sv
-/scratch/egor/bs34_dev/logic/dev/common/primitives/func/lib_mux_one_hot.sv
-/scratch/egor/bs34_dev/logic/dev/common/primitives/func/lib_ffs.sv
+../crc_byte_enable.sv
+../crc_parallel.sv
+# Add primitives
+#/func/lib_mux_one_hot.sv
+#/func/lib_ffs.sv
 
