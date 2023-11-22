2b52305831a92341b58f3bf4c22bc39bae097166
egorman44 <egorushka44@gmail.com>
2023-11-22 10:14:54 +0400

    [GF_MATH] Add gf package to share common functions in between modules.

--- rs/rtl/gf_mult.sv
+++ rs/rtl/gf_mult.sv
@@ -1,8 +1,5 @@
 module gf_mult
-  #(
-    parameter SYMB_WIDTH	= 8,
-    parameter POLY		= 285
-    )
+  import gf_pkg::*;
    (
     input [SYMB_WIDTH-1:0]  A,
     input [SYMB_WIDTH-1:0]  B,
@@ -11,43 +8,21 @@ module gf_mult
 
    localparam SYMB_NUM		= 2 ** SYMB_WIDTH;   
    
-   logic [SYMB_WIDTH-1:0] symb_to_alpha[1:0][SYMB_NUM-1:0];
-   logic [SYMB_WIDTH-1:0] alpha_to_symb[SYMB_NUM-1:0];
+   alpha_to_symb_t symb_to_alpha_tbl [1:0];
+   alpha_to_symb_t alpha_to_symb_tbl;
    
-   // Generate all alpha elements using LSFR rule.   
    always_comb begin
-      for(int i = 0; i < SYMB_NUM; ++i) begin
-	 if(i == 0)
-	   alpha_to_symb[0] = 1;
-	 else begin
-	    // If MSB of prev symb is 1 then POLY should be XORed with shifted
-	    // value because of the feedback
-	    if (alpha_to_symb[i-1][SYMB_WIDTH-1])
-	      alpha_to_symb[i]	= (alpha_to_symb[i-1] << 1) ^ POLY[SYMB_WIDTH-1:0];
-	    else
-	      alpha_to_symb[i]	= (alpha_to_symb[i-1] << 1);
-	 end
-      end
+      alpha_to_symb_tbl = alpha_to_symb();
+      for(int tbl_indx = 0; tbl_indx < 2; ++tbl_indx)
+	symb_to_alpha_tbl[tbl_indx] = symb_to_alpha(alpha_to_symb_tbl);
    end
    
-   // Inverse function.
-   // Note. There is no alpha_to_symb[x] = 0, thus make it manually
-   always_comb begin
-      for(int tbl_indx = 0; tbl_indx < 2; ++tbl_indx) begin
-	 symb_to_alpha[tbl_indx][0] = 0;	 
-	 for(int i = 0; i < SYMB_NUM; ++i) begin
-	    symb_to_alpha[tbl_indx][alpha_to_symb[i]] = i[SYMB_WIDTH-1:0];
-	 end
-      end
-   end
-   
-   // 1. Convert symbols to alpha:
-   
+   // 1. Convert symbols to alpha:   
    wire [SYMB_WIDTH-1:0] alpha_A, alpha_B;
-
-   assign alpha_A = symb_to_alpha[0][A];
-   assign alpha_B = symb_to_alpha[1][B];
-
+   
+   assign alpha_A = symb_to_alpha_tbl[0][A];
+   assign alpha_B = symb_to_alpha_tbl[1][B];
+   
    // 2. Sum up the powers of alpha_A and alpha_B in GF(2^SYMB_WIDTH):
 
    wire [SYMB_WIDTH-1:0] sum_alpha_AB = (alpha_A + alpha_B) % (SYMB_NUM-1);
@@ -59,7 +34,7 @@ module gf_mult
       if((A == 0) || (B == 0))
 	product = 0;
       else
-	product = alpha_to_symb[sum_alpha_AB];
+	product = alpha_to_symb_tbl[sum_alpha_AB];
    end
    
    assign P = product;
---
+++ rs/rtl/gf_pkg.sv
@@ -0,0 +1,46 @@
+package gf_pkg;
+
+`ifndef SYMB_WIDTH_DEF
+ `define SYMB_WIDTH_DEF 8
+ `define POLY_DEF 285
+`endif
+   
+   parameter SYMB_WIDTH = `SYMB_WIDTH_DEF;
+   parameter SYMB_NUM	= 2 ** SYMB_WIDTH;
+   parameter POLY	= `POLY_DEF;
+
+   typedef logic [SYMB_WIDTH-1:0] alpha_to_symb_t [SYMB_NUM-1:0];
+   
+   //////////////////////////////////////
+   // Generate all alpha elements using LSFR rule.   
+   //////////////////////////////////////
+   
+   function alpha_to_symb_t alpha_to_symb();
+      for(int i = 0; i < SYMB_NUM; ++i) begin
+	 if(i == 0)
+	   alpha_to_symb[0] = 1;
+	 else begin
+	    // If MSB of prev symb is 1 then POLY should be XORed with shifted
+	    // value because of the feedback
+	    if (alpha_to_symb[i-1][SYMB_WIDTH-1])
+	      alpha_to_symb[i]	= (alpha_to_symb[i-1] << 1) ^ POLY[SYMB_WIDTH-1:0];
+	    else
+	      alpha_to_symb[i]	= (alpha_to_symb[i-1] << 1);
+	 end
+      end // for (int i = 0; i < SYMB_NUM; ++i)
+   endfunction // alpha_to_symb
+
+   //////////////////////////////////////   
+   // Inverse function.
+   // Note. There is no alpha_to_symb[x] = 0, thus make it manually
+   //////////////////////////////////////
+   
+   function alpha_to_symb_t symb_to_alpha (alpha_to_symb_t alpha_to_symb);
+      symb_to_alpha[0] = 0;	 
+      for(int i = 0; i < SYMB_NUM; ++i) begin
+	 symb_to_alpha[alpha_to_symb[i]] = i[SYMB_WIDTH-1:0];
+      end
+   endfunction // symb_to_alpha
+   
+endpackage // gf_pkg
+   
