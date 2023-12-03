60df12503c5b1073d28fe964b2dafae19d81f3db
egorman44 <easmirnov44@gmail.com>
2023-12-04 00:29:00 +0400

    [RS_LIB] Add SyndrPredictor class.

--- rs/sim/rs_lib.py
+++ rs/sim/rs_lib.py
@@ -1,4 +1,8 @@
 from rs import rs_encode_msg
+from rs import rs_calc_syndromes
+
+from coco_env.scoreboard import Predictor
+
 from coco_env.packet import Packet
 
 class RsPacket(Packet):
@@ -9,9 +13,24 @@ class RsPacket(Packet):
         
     def gen_data(self, pattern):
         super().gen_data(pattern)
-        self.print_pkt()
         msg = self.get_byte_list()
         enc_msg = rs_encode_msg(msg, self.roots_num)
-        self.write_byte_list(enc_msg)
-        self.print_pkt()
+        self.write_byte_list(enc_msg)        
+
+
+class SyndrPredictor(Predictor):    
+
+    def __init__(self, name, port_prd, roots_num):
+        super().__init__(name, port_prd)
+        self.roots_num = roots_num
         
+    def predict(self):
+        for pkt in self.port_in:
+            syndrome = rs_calc_syndromes(pkt.get_byte_list(), self.roots_num)
+            syndrome.pop(0)
+            syndrome.reverse()
+            syndr_pkt = Packet(self.roots_num)
+            syndr_pkt.pkt_size = self.roots_num
+            syndr_pkt.write_byte_list(syndrome)
+            syndr_pkt.print_pkt(self.name)
+            self.port_prd.append(syndr_pkt)
