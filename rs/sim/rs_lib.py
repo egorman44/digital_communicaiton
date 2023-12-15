806b6144b8ae17627298e73e1039efb64af4e05d
egorman44 <easmirnov44@gmail.com>
2023-12-15 19:49:30 +0400

    [RS_LIB] Add packet corruption into RsPacket class. Add RsSyndromePacket, RsErrLocatorPacket classes.

--- rs/sim/rs_lib.py
+++ rs/sim/rs_lib.py
@@ -1,23 +1,50 @@
 from rs import rs_encode_msg
 from rs import rs_calc_syndromes
+from rs import rs_find_error_locator
 
 from coco_env.scoreboard import Predictor
-
 from coco_env.packet import Packet
 
 class RsPacket(Packet):
-
-    def __init__(self, roots_num, word_size = 1):
+    
+    def __init__(self, roots_num, word_size = 1, corrupt_words_num = 0):
         super().__init__(word_size)
         self.roots_num = roots_num
-        
-    def gen_data(self, pattern):
-        super().gen_data(pattern)
+        self.corrupt_words_num = corrupt_words_num
+    
+    def gen_data(self, pattern, ref_data = None):
+        # Generate origin message
+        super().gen_data(pattern, ref_data)
+        self.rs_gen_data()
+    
+    def rs_gen_data(self):
         msg = self.get_byte_list()
         enc_msg = rs_encode_msg(msg, self.roots_num)
-        self.write_byte_list(enc_msg)        
+        self.print_pkt("ENC_MSG")
+        self.write_byte_list(enc_msg)
+        if self.corrupt_words_num != 0:
+            self.corrupt_pkt(self.corrupt_words_num)
+        
+class RsSyndromePacket(RsPacket):
+    
+    def rs_gen_data(self):
+        #super().rs_gen_data()        
+        enc_msg = self.get_byte_list()
+        syndrome = rs_calc_syndromes(enc_msg, self.roots_num)
+        syndrome.pop(0)
+        syndrome.reverse()
+        self.pkt_size = self.roots_num
+        self.word_size = self.roots_num
+        self.write_byte_list(syndrome)
 
-
+class RsErrLocatorPacket(RsPacket):
+    def rs_gen_data(self):
+        #super().rs_gen_data()
+        enc_msg = self.get_byte_list()
+        syndrome = rs_calc_syndromes(enc_msg, self.roots_num)
+        error_locator = rs_find_error_locator(syndrome, self.roots_num)
+        print(f"error_locator {error_locator}")
+        
 class SyndrPredictor(Predictor):    
 
     def __init__(self, name, port_prd, roots_num):
@@ -34,3 +61,5 @@ class SyndrPredictor(Predictor):
             syndr_pkt.write_byte_list(syndrome)
             syndr_pkt.print_pkt(self.name)
             self.port_prd.append(syndr_pkt)
+
+#class ErrLocatorPredictor(Predictor):    
