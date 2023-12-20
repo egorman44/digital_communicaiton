a5ff09d6a412a37abd881b3d829646e6821bcb3f
egorman44 <easmirnov44@gmail.com>
2023-12-20 14:17:44 +0400

    [RS_LIB] Add RsErrPosPacket, ErrPositionPredictor classes.

--- rs/sim/rs_lib.py
+++ rs/sim/rs_lib.py
@@ -1,6 +1,8 @@
+import math
 from rs import rs_encode_msg
 from rs import rs_calc_syndromes
 from rs import rs_find_error_locator
+from rs import rs_find_errors
 
 from coco_env.scoreboard import Predictor
 from coco_env.packet import Packet
@@ -38,13 +40,38 @@ class RsSyndromePacket(RsPacket):
         self.write_byte_list(syndrome)
 
 class RsErrLocatorPacket(RsPacket):
+    
     def rs_gen_data(self):
+        t_len = math.floor(self.roots_num/2) + 1
         #super().rs_gen_data()
         enc_msg = self.get_byte_list()
         syndrome = rs_calc_syndromes(enc_msg, self.roots_num)
         error_locator = rs_find_error_locator(syndrome, self.roots_num)
-        print(f"error_locator {error_locator}")
+        error_locator.reverse()
+        if(len(error_locator) < t_len):
+               zeros = [0] * (t_len - len(error_locator))
+               error_locator = error_locator + zeros
+        self.pkt_size = t_len
+        self.word_size = t_len
+        self.write_byte_list(error_locator)
+
+class RsErrPosPacket(RsPacket):
+    
+    def rs_gen_data(self):
+        t_len = math.floor(self.roots_num/2) + 1
+        #super().rs_gen_data()
+        enc_msg = self.get_byte_list()
+        syndrome = rs_calc_syndromes(enc_msg, self.roots_num)
+        error_locator = rs_find_error_locator(syndrome, self.roots_num)                
+        error_location = rs_find_errors(error_locator,len(enc_msg))
+        #self.pkt_size = t_len
+        #self.word_size = t_len
+        #self.write_byte_list(error_locator)
         
+'''
+   *** PREDICTORS ***
+'''
+
 class SyndrPredictor(Predictor):    
 
     def __init__(self, name, port_prd, roots_num):
@@ -69,19 +96,40 @@ class ErrLocatorPredictor(Predictor):
         self.roots_num = roots_num
         
     def predict(self):
-        t_len = int(self.roots_num/2) + 1
+        t_len = math.floor(self.roots_num/2) + 1
         for pkt in self.port_in:
             syndrome = rs_calc_syndromes(pkt.get_byte_list(), self.roots_num)
-            err_locator = rs_find_error_locator(syndrome, self.roots_num)
-            print(f"err_locator = {err_locator}")
-            err_locator.reverse()
-            print(f"err_locator = {err_locator}")
-            if(len(err_locator) < t_len):
-               zeros = [0] * (t_len - len(err_locator))
-               err_locator = err_locator + zeros
-               print(f"err_locator = {err_locator}")
+            error_locator = rs_find_error_locator(syndrome, self.roots_num)
+            error_locator.reverse()
+            if(len(error_locator) < t_len):
+               zeros = [0] * (t_len - len(error_locator))
+               error_locator = error_locator + zeros
             syndr_pkt = Packet(t_len)
             syndr_pkt.pkt_size = t_len
-            syndr_pkt.write_byte_list(err_locator)
+            syndr_pkt.write_byte_list(error_locator)
             syndr_pkt.print_pkt(self.name)
             self.port_prd.append(syndr_pkt)
+
+class ErrPositionPredictor(Predictor):    
+
+    def __init__(self, name, port_prd, roots_num, n_len):
+        super().__init__(name, port_prd)
+        self.roots_num = roots_num
+        self.n_len = n_len
+        
+    def predict(self):
+        t_len = math.floor(self.roots_num/2) + 1
+        for pkt in self.port_in:
+            syndrome = rs_calc_syndromes(pkt.get_byte_list(), self.roots_num)
+            error_locator = rs_find_error_locator(syndrome, self.roots_num)
+            error_position_l = rs_find_errors(error_locator,self.n_len)
+            error_position = 0
+            xor_vector = (2 ** self.n_len)-1
+            for bit_pos in error_position_l:
+                error_position = error_position ^ (1 << bit_pos)            
+            error_position_bin = [error_position ^ xor_vector]
+            err_pos_pkt = RsErrPosPacket(self.roots_num)
+            err_pos_pkt.pkt_size = self.n_len
+            err_pos_pkt.write_byte_list(error_position_bin)
+            err_pos_pkt.print_pkt()
+            self.port_prd.append(err_pos_pkt)
