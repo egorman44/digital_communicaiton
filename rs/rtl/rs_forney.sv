a7566ad4229523ac0a13acc67b74669e37f011d8
egorman44 <easmirnov44@gmail.com>
2024-01-08 12:31:15 +0400

    [FORNEY] Update algo implementation.

--- rs/rtl/rs_forney.sv
+++ rs/rtl/rs_forney.sv
@@ -1,12 +1,16 @@
 module rs_forney
   import gf_pkg::*;
    (
-    input 		   aclk,
-    input 		   aresetn,
-    input [SYMB_WIDTH-1:0] error_positions[T_LEN-1:0],
-    input [T_LEN-1:0] 	   error_positions_vld,
-    input [SYMB_WIDTH-1:0] syndrome[ROOTS_NUM-1:0],
-    input 		   syndrome_vld
+    input 			  aclk,
+    input 			  aresetn,
+    input [SYMB_WIDTH-1:0] 	  error_positions[T_LEN-1:0],
+    input [T_LEN-1:0] 		  error_positions_vld,
+    input [SYMB_WIDTH-1:0] 	  syndrome[ROOTS_NUM-1:0],
+    input 			  syndrome_vld,
+    output logic [SYMB_WIDTH-1:0] magnitude [T_LEN-1:0],
+    output 			  magnitude_vld,
+    output 			  irq_error_magnitude
+    
     );
    
    // TODO: Get coef_pos
@@ -24,8 +28,11 @@ module rs_forney
    logic [T_LEN:0][SYMB_WIDTH-1:0] err_loc[T_LEN-1:0];
    
 
-   logic [SYMB_WIDTH-1:0]  synd_x_error_evaluator[ROOTS_NUM+T_LEN-1:0];
+   logic [SYMB_WIDTH-1:0] 	   synd_x_error_evaluator[ROOTS_NUM+T_LEN-1:0];
 
+   // TODO: what is the width [T_LEN:0] ?!   
+   wire [T_LEN:0][SYMB_WIDTH-1:0]  error_evaluator;
+   
    always_comb begin
       for(int i = 0; i < T_LEN; ++i) begin
 	 coef_position[i]	= N_LEN - 1 - error_positions[i];
@@ -111,12 +118,10 @@ module rs_forney
 	    if(symb_indx > word_indx)
 	      dividend_arr[word_indx][symb_indx] = '0;
 	    else
-	      dividend_arr[word_indx][symb_indx] = synd_x_error_evaluator[symb_indx];
+	      dividend_arr[word_indx][symb_indx] = synd_x_error_evaluator[word_indx-symb_indx];
 	 end
       end
    end
-
-   wire [T_LEN:0][SYMB_WIDTH-1:0] remainder;
    
    lib_mux_onehot
      #(
@@ -127,12 +132,15 @@ module rs_forney
      (
       .data_i(dividend_arr),
       .sel(error_positions_vld_msb),
-      .data_o(remainder)
+      .data_o(error_evaluator)
       );
 
    // Compute the formal derivative of the error locator polynomial
    logic [SYMB_WIDTH-1:0] 	  err_loc_prime_tmp [T_LEN-1:0][T_LEN-2:0];
-   logic [SYMB_WIDTH-1:0] 	  err_loc_prime [T_LEN-1:0][T_LEN-2:0]; 
+   logic [SYMB_WIDTH-1:0] 	  err_loc_prime_array [T_LEN-1:0][T_LEN-2:0];
+   logic [SYMB_WIDTH-1:0][T_LEN-1:0] err_loc_prime;
+   logic [SYMB_WIDTH-1:0][T_LEN-1:0] err_loc_prime_mux_in [T_LEN-1:0];
+   
    
    always_comb begin
       for(int x_inv_indx = 0; x_inv_indx < T_LEN; ++x_inv_indx) begin
@@ -143,11 +151,89 @@ module rs_forney
 	    else
 	      err_loc_prime_tmp[x_inv_indx][x_indx] = gf_mult(error_location_inv[x_inv_indx], error_location[x_indx+1]) ^ 1;
 	    if(x_indx == 0)
-	      err_loc_prime[x_inv_indx][x_indx] = err_loc_prime_tmp[x_inv_indx][x_indx];
+	      err_loc_prime_array[x_inv_indx][x_indx] = err_loc_prime_tmp[x_inv_indx][x_indx];
 	    else
-	      err_loc_prime[x_inv_indx][x_indx] = gf_mult(err_loc_prime[x_inv_indx][x_indx-1] , err_loc_prime_tmp[x_inv_indx][x_indx]);
+	      err_loc_prime_array[x_inv_indx][x_indx] = gf_mult(err_loc_prime_array[x_inv_indx][x_indx-1] , err_loc_prime_tmp[x_inv_indx][x_indx]);
+	 end // for (int x_indx = 0; x_indx < T_LEN-1; ++x_indx)
+	 //err_loc_prime[x_inv_indx] = err_loc_prime_array[x_inv_indx][x_inv_indx];
+      end // for (int x_inv_indx = 0; x_inv_indx < T_LEN; ++x_inv_indx)
+      for(int col_indx = 0; col_indx < T_LEN; ++col_indx) begin	 
+	 for(int row_indx = 0; row_indx < T_LEN; ++row_indx) begin
+	    if(col_indx == 0) begin
+	       err_loc_prime_mux_in[col_indx][row_indx] = 1;
+	    end
+	    else begin
+	       err_loc_prime_mux_in[col_indx][row_indx] = err_loc_prime_array[row_indx][col_indx-1];
+	    end
 	 end
+      end	      
+   end // always_comb
+
+   lib_mux_onehot
+     #(
+       .PORTS_NUMBER(T_LEN),
+       .WIDTH(T_LEN*SYMB_WIDTH)
+       )
+   err_loc_prime_mux
+     (
+      .data_i(err_loc_prime_mux_in),
+      .sel(error_positions_vld_msb),
+      .data_o(err_loc_prime)
+      );
+   
+   // TODO: add description for each block.
+   // gf_poly_eval(error_evaluator, Xi_inv)
+   logic [T_LEN-1:0][SYMB_WIDTH-1:0] xor_intrm [T_LEN:0];
+   logic [T_LEN-1:0][SYMB_WIDTH-1:0] gf_mult_intrm [T_LEN:0];
+   logic [T_LEN-1:0][SYMB_WIDTH-1:0] Yl;
+   
+   always_comb begin
+      for(int x_inv_indx = 0; x_inv_indx < T_LEN; ++x_inv_indx) begin
+	 for(int symb_indx = 0; symb_indx < T_LEN+1; ++symb_indx) begin
+	    if(symb_indx == 0) begin	       
+	       gf_mult_intrm[symb_indx][x_inv_indx]	= '0;
+	       xor_intrm[symb_indx][x_inv_indx]		= error_evaluator[symb_indx];
+	    end
+	    else begin
+	       gf_mult_intrm[symb_indx][x_inv_indx]	= gf_mult(xor_intrm[symb_indx-1][x_inv_indx], error_location_inv[x_inv_indx]);
+	       xor_intrm[symb_indx][x_inv_indx]		= gf_mult_intrm[symb_indx][x_inv_indx] ^ error_evaluator[symb_indx];
+	    end	    
+	 end
+      end // for (int symb_indx = 0; symb_indx < T_LEN; ++symb_indx)
+   end // always_comb
+
+   wire [T_LEN:0] gf_poly_eval_mux_sel = {error_positions_vld_msb, 1'b0};
+   
+   lib_mux_onehot
+     #(
+       .PORTS_NUMBER(T_LEN+1),
+       .WIDTH(T_LEN*SYMB_WIDTH)
+       )
+   gf_poly_eval_mux_inst
+     (
+      .data_i(xor_intrm),
+      .sel(gf_poly_eval_mux_sel),
+      .data_o(Yl)
+      );
+
+   // Adjust to fcr parameter
+   logic [SYMB_WIDTH-1:0] Yl_adjust [T_LEN-1:0];
+   logic [SYMB_WIDTH-1:0] gf_pow_adjust [T_LEN-1:0];
+   
+   always_comb begin
+      for(int symb_indx = 0; symb_indx < T_LEN; ++symb_indx) begin
+	 if(FIRST_ROOT == 1) begin
+	    gf_pow_adjust[symb_indx] = error_location[symb_indx];
+	 end
+	 else begin
+	    gf_pow_adjust[symb_indx] = pow_first_root_min1(error_location[symb_indx]);
+	 end
+	 Yl_adjust[symb_indx] = gf_mult(gf_pow_adjust[symb_indx], Yl[symb_indx]);
+	 magnitude[symb_indx] = gf_div(Yl_adjust[symb_indx], err_loc_prime[symb_indx]);
       end
-   end
+   end // always_comb
+
+   // err_loc_prime shouldn't be zero
+   assign irq_error_magnitude = ~(|err_loc_prime);
    
 endmodule // rs_forney
